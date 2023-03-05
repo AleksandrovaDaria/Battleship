2327fd5008d394b43aa27178ddafea38aa29679f
Daria Aleksandrova <106164543+AleksandrovaDaria@users.noreply.github.com>
2023-03-05 12:22:28 +0100

    add AI
    add AI

--- battleship.py
+++ battleship.py
@@ -91,15 +91,15 @@ def is_sunk(player_board_shooting, row, col,board_size):
         if col-1>0:
             if player_board_shooting[row][col-1] == "X":
                 iterator+=1
-            elif col+1<=board_size:
-                if player_board_shooting[row][col+1] == "X":
-                    iterator +=1
-                elif row+1<=board_size:  
-                    if player_board_shooting[row+1][col] == "X":
-                        iterator+=1
-                    elif row-1>0:
-                        if player_board_shooting[row-1][col] == "X":
-                            iterator+=1
+        if col+1<=board_size:
+            if player_board_shooting[row][col+1] == "X":
+                iterator +=1
+        if row+1<=board_size:  
+            if player_board_shooting[row+1][col] == "X":
+                iterator+=1
+        if row-1>0:
+            if player_board_shooting[row-1][col] == "X":
+                iterator+=1
     if iterator !=0:
         return False
     else:
@@ -128,11 +128,11 @@ def create_board(size):
 def get_ship_placement(board, size):
     while True:
         try:
-            position = input(f"Enter the row (A-{chr(65+len(board)-1)}) and column (1-{len(board)}) for a ship of size {size}: ")
+            position = input_with_quit(f"Enter the row (A-{chr(65+len(board)-1)}) and column (1-{len(board)}) for a ship of size {size}: ")
             row = ord(position[0]) - 65
             col = int(position[1:]) - 1
             if size !=1:
-                direction = input("Enter the direction (h/v): ")
+                direction = input_with_quit("Enter the direction (h/v): ")
             else:
                 direction="v"
             clear()
@@ -149,7 +149,7 @@ def get_ship_placement(board, size):
 def get_shot_position(board):
     while True:
         try:
-            position = input(f"Enter the row (A-{chr(65+len(board)-1)}) and column (1-{len(board)}) to shoot: ")
+            position = input_with_quit(f"Enter the row (A-{chr(65+len(board)-1)}) and column (1-{len(board)}) to shoot: ")
             row = ord(position[0]) - 65
             col = int(position[1:]) - 1
             if not (0 <= row < len(board) and 0 <= col < len(board[0])):
@@ -166,16 +166,17 @@ def play_game(board_size, ship_sizes):
         row, col, direction = get_ship_placement(board, size)
         place_ship(board, row, col, size, direction)
         display_board(board)
-    if input("Press enter to continue or q to quit: ") == "q":
-        print("Thank you game! Bay :(")
+    if input("Press enter to continue or \"quit\" to quit: ") == "quit":
+        print("Thank you for game! Bay :(")
         exit
     return board
 
 def shooting(player_board_shooting,board, player_active,board_size):
     print("Let's start shooting!")
     row, col = get_shot_position(player_board_shooting)
-    if check_shot(player_board_shooting,board, row, col,board_size):       
-        return shooting(player_board_shooting,board, player_active,board_size)
+    if not all_sunk(board):
+        if check_shot(player_board_shooting,board, row, col,board_size):       
+            return shooting(player_board_shooting,board, player_active,board_size)
     else:
         print(f"Congratulations! You've sunk all the ships! \n Winner is player {player_active}")
         exit()
@@ -183,12 +184,32 @@ def shooting(player_board_shooting,board, player_active,board_size):
 def ai_play_game(board_size, ship_sizes):
     board = create_board(board_size)
     for size in ship_sizes:
-        row = random.randint(0,board_size)
-        col= random.randint(0,board_size)
-        directio
-        place_ship(board, row, col, size, direction)
+        row = random.randint(0,board_size-1)
+        col= random.randint(0,board_size-1)
+        direction=random.choice(("v", "h"))
+        if can_place_ship(board, row, col, direction, size):
+            place_ship(board, row, col, size, direction)
+        else:
+            return ai_play_game(board_size, ship_sizes)
     display_board(board)
-    if input("Press enter to continue or q to quit: ") == "q":
-        print("Thank you game! Bay :(")
+    if input_with_quit("Press enter to continue or \"quit\" to quit: ") == "quit":
+        print("Thank you for game! Bay :(")
         exit
     return board
+
+def shooting_ai(player_board_shooting,board, player_active,board_size):
+    row = random.randint(0,board_size-1)
+    col= random.randint(0,board_size-1)
+    if not all_sunk(board):
+        if check_shot(player_board_shooting,board, row, col,board_size):       
+            return shooting_ai(player_board_shooting,board, player_active,board_size)
+    else:
+        print(f"Congratulations! You've sunk all the ships! \n Winner is player {player_active}")
+        exit()
+
+def input_with_quit(question = ""):
+    answer = input(question)
+    if answer.lower() == "quit":
+        print("Thank you for game! Bay :(")
+        exit()
+    return answer
